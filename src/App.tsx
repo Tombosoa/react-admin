de075aa42615b6b2db9bc1115f52b1832ba136b6
Tombosoa <hei.nantenaina.4@gmail.com>
2024-04-28 21:35:03 +0300

    feat: add a changes on app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser } from 'react-admin';
 import { dataProvider } from './providers/dataProvider';
+import post from './operations/post';
 import comments from './operations/comments';
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name='users' list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+    <Resource name='posts' {...post} />
     <Resource name='comments' {...comments} />
   </Admin>
 );
